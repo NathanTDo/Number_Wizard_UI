97aa80213815822c8912395f14c17ccdda409eca
NathanTDo <nathantdo04@gmail.com>
2020-07-03 07:33:44 -0700

    Added NumberWizard console code

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿//using system.collections;
+//using system.collections.generic;
+//using system.diagnostics;
+//using System.Diagnostics;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+        max = max + 1;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+    }
+}
